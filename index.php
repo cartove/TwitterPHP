6e710c392dd3a40162f5b5d50e23646ea6f81219
Muhammad Negm <alam.hor@gmail.com>
2014-08-08 22:04:12 +0200

    Now working like fuck

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Twitter</title>
+    <link href="assets/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link rel="stylesheet" href="assets/style.css">
+  </head>
+  <body class="cartove" dir="ltr">
+        <?php include 'inc/twitterWidget.inc'; ?>
+  </body>
+</html>
