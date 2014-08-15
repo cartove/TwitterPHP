11d6c3effe5d06d9ab776dfd346579000d829444
Muhammad Negm <alam.hor@gmail.com>
2014-08-15 20:36:41 +0200

    AJAX every 5 seconds

--- inc/twitterWidget.php
+++ inc/twitterWidget.php
@@ -17,7 +17,7 @@
                   success: function(response){
                       document.getElementById('tw-list').innerHTML += response;
                   }
-              });}, 1000);
+              });}, 10000);
     </script>
     </ol>
   </div>
