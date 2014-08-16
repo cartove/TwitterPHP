6615a88b2d84d904aa4452744da03e7012081f75
Muhammad Negm <alam.hor@gmail.com>
2014-08-16 02:17:52 +0200

    loads first time then second time, also fuck that shit, cacing 101 was added

--- inc/twitterWidget.php
+++ inc/twitterWidget.php
@@ -8,6 +8,14 @@
     <ol class="stream-items" id="tw-list">
     <?php // include_once ('tweets_cached.php'); // Databse ?>
     <script type="text/javascript">
+      $.ajax({
+                  type: "POST",
+                  url: "inc/tweets.php",
+                  dataType:'text',
+                  success: function(response){
+                      document.getElementById('tw-list').innerHTML = response + document.getElementById('tw-list').innerHTML;
+                  }
+              });
 
         setInterval(function()
             { 
@@ -18,7 +26,7 @@
                   success: function(response){
                       document.getElementById('tw-list').innerHTML = response + document.getElementById('tw-list').innerHTML;
                   }
-              });}, 5000);
+              });}, 10000);
     </script>
     </ol>
   </div>
